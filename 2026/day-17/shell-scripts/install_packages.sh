45c2d3dba7f6cf49494a2218def4784d0c456280
Alok-Nayak <aloknayak.contact@gmail.com>
2026-06-03 23:24:30 +0530

    day-17 package-install-script

--- 2026/day-17/shell-scripts/install_packages.sh
+++ 2026/day-17/shell-scripts/install_packages.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-packages=('nginx' 'curl' 'wget')
+read -p "Enter the PackageName You Want To Install: " package
 
-for pkg in ${packages[@]}; do
-	 echo "Checking status for: $pkg"
-
-	 if  command -v $pkg  > /dev/null 2>&1; then
-		 echo "Status: $pkg is ALREADY installed. Skipping..."
-	else
-		echo "Status: $pkg is MISSING. Attempting install..."
-	fi
-
-done
+if [ "$package" == "" ]; then
+        echo "Please Enter The Package Name !!"
+elif command -v "$package" > /dev/null 2>&1; then
+        echo "$package is already installed!"
+else
+        echo "Installing $package ..."
+        echo "Updating Package list..."
+        sudo apt-get update > /dev/null 2>&1
+        sudo apt-get install "$package" -y > /dev/null 2>&1
+        
+        echo "Checking if it's installed..."
+        if command -v "$package" > /dev/null 2>&1; then
+            echo "Success: $package is now installed!"
+        else
+            echo "Error: Failed to install $package."
+        fi
+fi
 
